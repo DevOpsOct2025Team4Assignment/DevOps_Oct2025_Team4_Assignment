e8da76536bc7d5b23542ad0731d1143fa857d8c9
Haziq Hairil <haziqhairil21@gmail.com>
2026-02-03 17:46:10 +0800

    Clean up Flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,29 @@
-from .setup import app
-from . import admin
+import os
+
+from dotenv import load_dotenv
+from flask import Flask
+
+load_dotenv()
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get("SECRET_KEY", "dev"),
+        DATABASE=os.path.join(app.instance_path, "database.sqlite"),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    from . import admin, auth, db
+
+    db.init_app(app)
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(admin.bp)
+
+    return app
