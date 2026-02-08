c58b833fdb38a9b73bcdfdf279746ef63c05266e
ash <ashtonleu@gmail.com>
2026-02-08 11:24:02 +0800

    updated logging to use CLF

--- app/devops_logger.py
+++ app/devops_logger.py
@@ -1,18 +1,25 @@
 import logging
-import os
+from flask import request
+from datetime import datetime
 
-
+# Setup the logger
 devops_logger = logging.getLogger('devsecops')
 devops_logger.setLevel(logging.INFO)
 
 
-file_handler = logging.FileHandler('devsecops_audit.log')
-formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(topic)s | %(message)s')
-file_handler.setFormatter(formatter)
-
+file_handler = logging.FileHandler('access.log') 
 devops_logger.addHandler(file_handler)
 
 def log_security_event(topic, message):
-    """Logs events like login failures, admin actions, or unauthorized access."""
-    devops_logger.info(message, extra={'topic': topic})
+    """
+    Formatted for Common Log Format (CLF)
+    Format: IP - - [Time] "TOPIC Message" STATUS
+    """
 
+    timestamp = datetime.now().strftime('%d/%b/%Y:%H:%M:%S +0000')
+    ip = request.remote_addr if request else "127.0.0.1"
+    
+
+    log_line = f'{ip} - - [{timestamp}] "{topic} {message}"'
+    
+    devops_logger.info(log_line)
